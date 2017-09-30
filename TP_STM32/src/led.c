8123fc12f50e7a6b0585020cfa3d3833c95148d7
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-09-30 22:45:47 +0200

    led manager functions (blink, pwm)

---
+++ TP_STM32/src/led.c
@@ -0,0 +1,105 @@
+#include "ch.h"
+#include "hal.h"
+#include "led.h"
+
+// Blinking period
+static int period;
+
+/* State of the LED;
+** 0: off
+** 1: on
+*/
+static int state = 0;
+
+static virtual_timer_t led_timer;
+
+/* PWM callbacks functions
+*/
+static void pwm_callback(__attribute__((unused)) PWMDriver *pwmp)
+{
+    if(state)
+        LED_ON();
+}
+
+static void channel_callback(__attribute__((unused)) PWMDriver *pwmp)
+{
+    LED_OFF();
+}
+
+/* LED blinking callback
+*/
+static void led_blink_callback(__attribute__((unused)) void * p)
+{
+    // Restart timer
+    chSysLockFromISR();
+    chVTSetI(&led_timer, MS2ST(period), led_blink_callback, NULL);
+    chSysUnlockFromISR();
+    // Toggle LED
+    led_toggle();
+ }
+
+/* Configure a pwm at 120Hz
+** It's a common value in order to be confortable for eyes
+** 12000 was choosen for frequency because this assertions must be verified:
+** clk / frequency - 1 < 0xFFFF, with clk = 84000000, i.e frequency > 1282Hz
+** Moreover, I choosed frequency which verify 120*10^x for clarity
+*/
+static PWMConfig pwmcfg = {
+    12000,                          // frequency
+    100,                            // PWM period
+    pwm_callback,                   // callback called each period
+   {{PWM_OUTPUT_DISABLED, channel_callback}, // first channel init, material output disabled but callback called each rising edge
+    {PWM_OUTPUT_DISABLED, NULL},
+    {PWM_OUTPUT_DISABLED, NULL},
+    {PWM_OUTPUT_DISABLED, NULL}},
+    0,                              // TIM CR2 register init data
+    0                               // TIM DIER register init data
+};
+
+void led_init(void)
+{
+    LED_SET_PIN();
+    led_off();
+}
+
+void led_on(void)
+{
+    LED_ON();
+    state = 1;
+}
+
+void led_off(void)
+{
+    LED_OFF();
+    state = 0;
+}
+
+void led_toggle(void)
+{
+    LED_TOGGLE();
+    state = 1 - state;
+}
+
+void led_pwm(unsigned int intensity)
+{
+    if(intensity > 100)
+        return;
+    pwmStart(&PWMD3, &pwmcfg);
+    // Set PWM duty cycle
+    pwmEnableChannel(&PWMD3, 0, intensity);
+    // Enable channel callbacks
+    pwmEnableChannelNotification(&PWMD3, 0);
+    // Enable PWM callbacks
+    pwmEnablePeriodicNotification(&PWMD3);
+}
+
+void led_blink(unsigned int m_period)
+{
+    if(m_period < 10 || m_period > 10000)
+        return;
+    period = m_period / 2;
+    // Init timer
+    chVTObjectInit(&led_timer);
+    // Start timer
+    chVTSet(&led_timer, MS2ST(period), led_blink_callback, NULL);
+}
--- TP_STM32/src/main.c
+++ TP_STM32/src/main.c
@@ -2,34 +2,6 @@
 #include "hal.h"
 #include "led.h"
 
-static void pwm_callback(__attribute__((unused)) PWMDriver *pwmp)
-{
-    led_on();
-}
-
-static void channel_callback(__attribute__((unused)) PWMDriver *pwmp)
-{
-    led_off();
-}
-
-/* Configure a pwm at 120Hz
-** It's a common value in order to be confortable for eyes
-** 12000 was choosen for frequency because this assertions must be verified:
-** clk / frequency - 1 < 0xFFFF, with clk = 84000000, i.e frequency > 1282Hz
-** Moreover, I choosed frequency which verify 120*10^x for clarity
-*/
-static PWMConfig pwmcfg = {
-    12000,                          // frequency
-    100,                            // PWM period
-    pwm_callback,                   // callback called each period
-   {{PWM_OUTPUT_DISABLED, channel_callback}, // first channel init, material output disabled but callback called each rising edge
-    {PWM_OUTPUT_DISABLED, NULL},
-    {PWM_OUTPUT_DISABLED, NULL},
-    {PWM_OUTPUT_DISABLED, NULL}},
-    0,                              // TIM CR2 register init data
-    0                               // TIM DIER register init data
-};
-
 int main(void)
 {
     /*
@@ -43,14 +15,8 @@ int main(void)
 
     // Configure LED
     led_init();
-
-    pwmStart(&PWMD3, &pwmcfg);
-    // Set PWM duty cycle
-    pwmEnableChannel(&PWMD3, 0, 20); // 20% of PWM period
-    // Enable channel callbacks
-    pwmEnableChannelNotification(&PWMD3, 0);
-    // Enable PWM callbacks
-    pwmEnablePeriodicNotification(&PWMD3);
+    led_pwm(10);
+    led_blink(1000);
 
     while(1)
         chThdSleep(TIME_INFINITE);
