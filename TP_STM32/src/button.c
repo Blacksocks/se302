4ff80b79a339f5d344d6baeab934b908924bc35e
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-10-09 10:01:29 +0200

    handle button click

--- TP_STM32/src/button.c
+++ TP_STM32/src/button.c
@@ -5,10 +5,14 @@
 
 #define GET_PUSHBTN()     palReadPad(GPIOA, GPIOA_BUTTON_WKUP)
 #define KICKBACK_DELAY    5
+#define BTN_CLICK_DELAY   200
 
 // Used to remove kickback
 static int ready = 1;
 static virtual_timer_t kickback_timer;
+// Used to handle button click
+static int click_ready = 1;
+static virtual_timer_t click_timer;
 
 static void btn_up_handler(void)
 {
@@ -20,15 +24,47 @@ static void btn_down_handler(void)
     chprintf((BaseSequentialStream *)&RTT_stream, "[INFO] btn down\r\n");
 }
 
+static void btn_click_handler(void)
+{
+    chprintf((BaseSequentialStream *)&RTT_stream, "[INFO] btn click\r\n");
+}
+
+/* Triggered when the button is pressed or released without kickback
+*/
+static void click_callback(void * p)
+{
+    (void)p;
+    if(GET_PUSHBTN() == PAL_LOW) {
+        if(click_ready)
+            btn_up_handler();
+        else
+            btn_click_handler();
+    }
+    else
+        btn_down_handler();
+    click_ready = 1;
+}
+
 /* Triggered when the button is pressed or released without kickback
 */
 static void btn_callback(void * p)
 {
     (void)p;
-    if(GET_PUSHBTN() == PAL_LOW)
-        btn_up_handler();
-    else
-        btn_down_handler();
+    // Handle three events; button pressed, released or clicked
+    if(click_ready && GET_PUSHBTN() == PAL_HIGH) {
+        // Button pressed
+        click_ready = 0;
+        chSysLockFromISR();
+        chVTSetI(&click_timer, MS2ST(BTN_CLICK_DELAY), click_callback, NULL);
+        chSysUnlockFromISR();
+    }
+    else if(GET_PUSHBTN() == PAL_LOW) {
+        // Button released
+        chSysLockFromISR();
+        chVTResetI(&click_timer);                                                        \
+        chSysUnlockFromISR();
+        click_callback(NULL);
+    }
     ready = 1;
 }
 
@@ -38,6 +74,8 @@ static void btn_raw_handler(EXTDriver *extp, expchannel_t channel)
 {
     (void)extp;
     (void)channel;
+    // Hide all button event until kickback delay is not reached
+    // Therefore, only one button callback function is called when button is pressed or released
     if(ready) {
         chSysLockFromISR();
         chVTSetI(&kickback_timer, MS2ST(KICKBACK_DELAY), btn_callback, NULL);
