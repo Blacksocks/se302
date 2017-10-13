5169be7833893672aa914f01045045d8d9f90f25
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-10-13 14:29:55 +0200

    fix some errors

--- challenge2018/src/lcd.c
+++ challenge2018/src/lcd.c
@@ -1,10 +1,12 @@
 #include "lcd.h"
+#include "ch.h"
+#include "hal.h"
 
-#define LCD_RESET I2C1_SCL  // PB8
-#define LCD_DC    I2C1_SDA  // PB9
-#define LCD_MOSI  SPI2_MOSI // PC3
-#define LCD_SCK   SPI2_SCK  // PB10
-#define LDC_CS    SPI2_CS   // PG10
+#define LCD_RESET GPIOB_I2C1_SCL  // PB8
+#define LCD_DC    GPIOB_I2C1_SDA  // PB9
+#define LCD_MOSI  GPIOC_SPI2_MOSI // PC3
+#define LCD_SCK   GPIOB_SPI2_SCK  // PB10
+#define LCD_CS    GPIOG_SPI2_CS   // PG10
 
 #define LCD_RESET_LOW()     palClearPad(GPIOB, LCD_RESET)
 #define LCD_RESET_HIGH()    palSetPad(GPIOB, LCD_RESET)
@@ -14,7 +16,7 @@ static void set_lcd_pins(void)
 {
     palSetPadMode(GPIOB, LCD_RESET, PAL_MODE_OUTPUT_PUSHPULL);
     palSetPadMode(GPIOB, LCD_DC, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPadMode(GPIOC, LCD_MOSI, PAL_MODE_ALTERNATE(5))
+    palSetPadMode(GPIOC, LCD_MOSI, PAL_MODE_ALTERNATE(5));
     palSetPadMode(GPIOB, LCD_SCK, PAL_MODE_ALTERNATE(5));
     palSetPadMode(GPIOG, LCD_CS, PAL_MODE_OUTPUT_PUSHPULL);
 }
@@ -29,5 +31,5 @@ void init_lcd(void)
     chThdSleepMilliseconds(15);
     LCD_RESET_HIGH();
     chThdSleepMilliseconds(15);
-    chThdSleepMilliseconds(15);
+
 }
