d5c1ad1f1d9ac8f26feda8ab06e7a5091f4a1cb3
Vincent Gaillard <vincent.gaillard@telecom-paristech.fr>
2017-10-13 14:26:46 +0200

    set pins

--- challenge2018/src/lcd.c
+++ challenge2018/src/lcd.c
@@ -6,12 +6,28 @@
 #define LCD_SCK   SPI2_SCK  // PB10
 #define LDC_CS    SPI2_CS   // PG10
 
+#define LCD_RESET_LOW()     palClearPad(GPIOB, LCD_RESET)
+#define LCD_RESET_HIGH()    palSetPad(GPIOB, LCD_RESET)
+#define LCD_CS_LOW()        palClearPad(GPIOG, LCD_CS)
+
 static void set_lcd_pins(void)
 {
-
+    palSetPadMode(GPIOB, LCD_RESET, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOB, LCD_DC, PAL_MODE_OUTPUT_PUSHPULL);
+    palSetPadMode(GPIOC, LCD_MOSI, PAL_MODE_ALTERNATE(5))
+    palSetPadMode(GPIOB, LCD_SCK, PAL_MODE_ALTERNATE(5));
+    palSetPadMode(GPIOG, LCD_CS, PAL_MODE_OUTPUT_PUSHPULL);
 }
 
 void init_lcd(void)
 {
     set_lcd_pins();
+    LCD_CS_LOW();
+
+    // Init procedure
+    LCD_RESET_LOW();
+    chThdSleepMilliseconds(15);
+    LCD_RESET_HIGH();
+    chThdSleepMilliseconds(15);
+    chThdSleepMilliseconds(15);
 }
